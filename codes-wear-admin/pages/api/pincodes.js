85e8c70430bcd198c0b071258820df9623054f05
Raman Nagar <115226318+Raman-Nagar@users.noreply.github.com>
2023-03-07 16:21:32 +0530

    Update pincodes.js

--- codes-wear-admin/pages/api/pincodes.js
+++ codes-wear-admin/pages/api/pincodes.js
@@ -1,6 +1,13 @@
 import Pincodes from '../../pincodes.json';
+import NextCors from 'nextjs-cors';
 
-export default function handler(req, res) {
-  // let pincodes = 
+export default async function handler(req, res) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   res.status(200).json(Pincodes);
 }
